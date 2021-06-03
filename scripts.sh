da1b22b2077015a249563778f5c37fcde597f46d
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2021-06-03 00:47:23 -0300

    Add list todos from user

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,5 @@
 # create user
 curl -X POST http://localhost:3333/users -H 'content-type: application/json'  --data '{"name": "Jerson", "username": "jersonsatoru"}'
 
+# list users
+curl http://localhost:3333/todos
--- src/index.js
+++ src/index.js
@@ -11,8 +11,16 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { name } = request.headers;
-  return users.some((u) => u.name === name);
+  const { username } = request.headers;
+  const user = users.find((u) => u.username === username);
+  if (!user) {
+    return response.status(404).json({
+      message: 'User not found',
+    });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -30,7 +38,8 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
